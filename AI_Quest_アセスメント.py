97dc1499986ff2bed3bd3cc9b8177bf6d51452c7
Kei <dkk.1286@icloud.com>
2020-08-23 15:26:47 +0900

    対数正規ver.　スコア悪い。　なぜだ

--- AI_Quest_アセスメント.py
+++ AI_Quest_アセスメント.py
@@ -90,10 +90,49 @@ profile.to_file(outputfile="signate_ai_quest_train_y_over750.html")
 #         - 決定木の分岐の繰り返しによって変数間の相互作用を反映する。
 #
 # ## next
+# - model ver.2
+#     - 目的変数をlog(y)にする:目的変数は対数正規分布なので。
 
-test[cat_cols]
+# + jupyter={"outputs_hidden": true}
+train
+y = train['y'].values
+train['logY'] = np.log(y)
+train
+
+# + jupyter={"outputs_hidden": true}
+#y histgram
+# %matplotlib inline 
+plt.hist(train['y'], bins=50)
+
+# + jupyter={"outputs_hidden": true}
+#logY histgram
+# %matplotlib inline 
+plt.hist(train['logY'], bins=50)
 
 # +
+#QQプロット
+'''
+QQプロットは、X軸上に観測した累積パーセント、
+Y軸上に期待累積パーセントを持つグラフで、
+一直線上になっていれば正規分布になっていることがわかる。
+'''
+import scipy.stats as stats
+import pylab
+stats.probplot(train['y'], dist="norm", plot=pylab)
+plt.show()
+
+stats.probplot(train['logY'], dist="norm", plot=pylab)
+#正規分布に近づいているので直線ぽくなっている。
+plt.show()
+
+# + jupyter={"outputs_hidden": true}
+train['y']
+# -
+
+# ## model ver.1
+
+# +
+#model ver.1
 from sklearn.model_selection import train_test_split  
 from sklearn.preprocessing import LabelEncoder#カテゴリ変数をループしてlabelencoding
 
@@ -126,12 +165,62 @@ model.fit(x_train, y_train)
 
 # 予測  
 y_pred = model.predict(x_validation)  
+y_pred
 # -
-# # 3.validation
+# ## model ver.2
 
-# ### 3.1mse・rmseの計算
+y
 
 # +
+#model ver.2
+from sklearn.model_selection import train_test_split  
+from sklearn.preprocessing import LabelEncoder#カテゴリ変数をループしてlabelencoding
+
+columns = ['accommodates', 'bathrooms', 'bedrooms', 'beds',
+           'review_scores_rating','cleaning_fee','instant_bookable',
+           'bed_type','cancellation_policy','city']
+
+cat_cols = ['cleaning_fee','instant_bookable','bed_type',
+           'cancellation_policy','city']
+
+
+
+
+for c in cat_cols:#学習データに基づいて定義する
+    le=LabelEncoder()
+    le.fit(train[c])
+    train[c]=le.transform(train[c])
+    test[c]=le.transform(test[c])
+    
+x = train[columns]#数値データかつcorrelationが高い
+y = train['logY']
+
+x_train, x_validation, y_train, y_validation = train_test_split(x, y, train_size=0.8, random_state=123)
+# -
+
+y_train
+
+# +
+from xgboost import XGBRegressor
+# 学習  
+model2 = XGBRegressor(objective="reg:squarederror")  
+model2.fit(x_train, y_train)  
+
+# 予測  
+logY_pred = model2.predict(x_validation)  
+y_pred2 = np.exp(logY_pred) 
+# -
+
+y_pred2
+
+logY_pred
+
+# # 3.validation
+
+# ## 3.1.1 model ver.1
+
+# +
+#modelver.1
 from sklearn.model_selection import cross_val_score
 
 #交差検証
@@ -143,6 +232,9 @@ print('Average score: {}'.format(np.mean(-rmse)))
 # -
 
 
+y_pred = np.exp(logY_pred) 
+
+
 #フツーの検証
 from sklearn.metrics import mean_squared_error 
 import numpy as np
@@ -151,8 +243,31 @@ rmse=np.sqrt(mse)
 print(f"mse:{mse}")  
 print(f"rmse:{rmse}")
 
+# ### 3.1.2model ver.2
 
-# ### 3.2予想値と実測値のグラフを描写
+from sklearn.metrics import mean_squared_error 
+import numpy as np
+mse=mean_squared_error(y_validation, y_pred2)  
+rmse=np.sqrt(mse)
+print(f"mse:{mse}")  
+print(f"rmse:{rmse}")
+
+# cross validaitonの仕方わからん。
+
+# +
+from sklearn.model_selection import cross_val_score
+
+#交差検証
+rmse = cross_val_score(model2, x_train, ????, scoring='neg_root_mean_squared_error')
+print('Cross-Validation scores: {}'.format(-rmse))
+# スコアの平均値
+import numpy as np
+print('Average score: {}'.format(np.mean(-rmse)))
+# -
+
+# ## 3.2予想値と実測値のグラフを描写
+
+# ### 3.2.1 model ver.1
 
 # %matplotlib inline
 #feature importance のプロット
@@ -199,7 +314,7 @@ plt.legend(loc="best")
 plt.show()
 
 # +
-#予測値(predY_array)と実測値(Y_arra)の差分を確認
+#予測値(predY_array)と実測値(Y_arra)の差分(diff)を確認
 
 predY_array=np.array(y_pred.tolist())
 Y_array=np.array(y_validation.tolist())
@@ -208,11 +323,29 @@ diff = Y_array- predY_array
 diff = abs(diff)#絶対値にする
 # -
 
+#diffの情報をみる
+diff_series = pd.Series(diff)
+desc = diff_series.describe()
+median=diff_series.median()
+print(f"median:{median}")
+print(f"{desc}")
+
+# 中央値は40だけど、平均は66、maxが1308
+# だいぶ大きい誤差のやつに引きずられている。
+#
+# 評価尺度がrmseなので、
+# ➡︎今回のポイントは値段の高いhotelの予測をいかにするかがキモ
+
+#棒グラフ
 # %matplotlib inline
 x = np.arange(len(diff))
 plt.bar(x,diff)
 
-# ###　上記図これぐらい差が出ていると言うことは、そもそも高い金額のホテルを当てることができていないと思われる。理由として対数正規分布があり、サンプル数が少ないところに対するアプローチが必要。
+#ヒストグラム
+# %matplotlib inline 
+plt.hist(diff, bins=50)
+
+# 上記図これぐらい差が出ていると言うことは、そもそも高い金額のホテルを当てることができていないと思われる。理由として対数正規分布があり、サンプル数が少ないところに対するアプローチが必要。
 
 # # 4.submit
 
@@ -220,22 +353,29 @@ test.head()
 
 
 
+
+
 test_x = test[columns]
 
 test_x
 
-test_y_pred = model.predict(test_x)  
+test_logY_pred = model2.predict(test_x)  
+test_pred2 = np.exp(test_logY_pred) 
 
-test_y_pred_list = test_y_pred.tolist()
-len(test_y_pred_list)
+test_y_pred2_list = test_pred2.tolist()
+len(test_y_pred2_list)
 
 sample[1] = test_y_pred_list
 
 sample
 
-sample.to_csv('submit_ver.2.csv', header=False, index=False)
+sample[1] = test_y_pred2_list
 
-sub1 = pd.read_csv("submit_ver.2.csv")
+sample
+
+sample.to_csv('submit_ver.3.csv', header=False, index=False)
+
+sub1 = pd.read_csv("submit_ver.3.csv")
 sub1
 
 
